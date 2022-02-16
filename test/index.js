4ef1c9f165bae576df78dfd7600a470c935edcd0
5C24 <you_5c24@163.com>
2022-02-16 10:47:01 +0800

    中间件使用

--- test/index.js
+++ test/index.js
@@ -1,8 +1,23 @@
-const StaticServer = require("../src/index");
+const compose = require("../src/utils/compose");
+const middleware = [];
 
-const staticServer = new StaticServer({
-  port: 9527,
-  root: "H://workspace_5c24//static-server",
+middleware.push(async (ctx, next) => {
+  console.log("第 1 个中间件 next 前");
+  await next();
+  console.log("第 1 个中间件 next 后");
 });
 
-staticServer.start();
+middleware.push(async (ctx, next) => {
+  console.log("第 2 个中间件 next 前");
+  // await next(); // 不调用 next()
+  console.log("第 2 个中间件 next 后");
+});
+
+middleware.push(async (ctx, next) => {
+  console.log("第 3 个中间件 next 前");
+  await next();
+  console.log("第 3 个中间件 next 后");
+});
+
+const ctx = {};
+compose(middleware)(ctx);
